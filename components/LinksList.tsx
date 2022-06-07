05a57697206addc266929137a7c346145d12a927
Aaron Styles <aaron@charcoalstyles.com>
2022-06-07 11:44:46 +1000

    UI updates

--- components/LinksList.tsx
+++ components/LinksList.tsx
@@ -1,4 +1,4 @@
-import { Grid, IconButton, Typography } from "@mui/material";
+import { Grid, IconButton, styled, Typography } from "@mui/material";
 import Link from "next/link";
 import { Link as LinkData } from "@prisma/client";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -6,11 +6,23 @@ import EditIcon from "@mui/icons-material/Edit";
 import axios from "axios";
 import { useState } from "react";
 import { LinkModal } from "./LinkModal";
+import { format } from "date-fns";
+import { colours } from "../src/theme";
 
 type LinksListProps = {
   links: LinkData[];
   updateLinks: (links: LinkData[]) => void;
 };
+
+const LinkText = styled(Typography)(() => ({
+  color: colours.dark[1],
+  transition: "color 0.2s ease-in-out",
+
+  "&:hover": {
+    cursor: 'pointer',
+    color: colours.light[1],
+  },
+}));
 
 export const LinksList = ({ links, updateLinks }: LinksListProps) => {
   const [isEditing, setIsEditing] = useState(false);
@@ -37,23 +49,39 @@ export const LinksList = ({ links, updateLinks }: LinksListProps) => {
         {links.map((link) => {
           return (
             <Grid item key={link.id} borderBottom={2} paddingBottom={1}>
-              <Grid container flexDirection="row" alignItems="center" justifyContent="space-between">
+              <Grid
+                container
+                flexDirection="row"
+                alignItems="center"
+                justifyContent="space-between"
+              >
                 <Grid item>
-                  <Link href={link.url}>
-                    <Typography variant="h3">{link.title}</Typography>
-                  </Link>
-                  <Grid container>
+                  <Grid container flexDirection="column">
+                    <Link href={link.url}>
+                      <LinkText variant="h3">{link.title}</LinkText>
+                    </Link>
                     <Grid item>
                       <Typography variant="body1">{link.url}</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography variant="body2">
+                        {format(
+                          new Date(link.createdAt),
+                          "yyyy-MM-dd HH:mm:ss(xxx)"
+                        )}
+                      </Typography>
                     </Grid>
                   </Grid>
                 </Grid>
 
                 <Grid item>
-                  <IconButton size="large" onClick={() => {
-                    setEditingLink(link);
-                    setIsEditing(true);
-                  }}>
+                  <IconButton
+                    size="large"
+                    onClick={() => {
+                      setEditingLink(link);
+                      setIsEditing(true);
+                    }}
+                  >
                     <EditIcon />
                   </IconButton>
                   <IconButton
